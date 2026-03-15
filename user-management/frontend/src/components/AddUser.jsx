dd838968938b5d834161b9a8572daedd3aa5c279
nithin-6546 <mudumalanithin6@gmail.com>
2026-03-15 12:50:34 +0530

    Update AddUser.jsx

--- user-management/frontend/src/components/AddUser.jsx
+++ user-management/frontend/src/components/AddUser.jsx
@@ -19,7 +19,7 @@ function AddUser() {
     setServerError(""); 
     
     try {
-      let res = await fetch("https://user-management-app-1-z9lx.onrender.com/user-api", {
+      let res = await fetch("https://user-management-app-1-z9lx.onrender.com/user-api/user-list", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(newUser),
